c1514064d6b41040f5976052e68f5b5e94249d20
Angelo31294 <luongoangelo1@gmail.com>
2022-10-27 15:40:11 +0200

    Fine

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ const containerList = document.getElementById("list");
 //Creo un elemento "li" per ogni elemento dell'array
 let i = 0;
 
-while(i = shopList.length){
+while(i < shopList.length){
     const listitem = document.createElement("li");
     listitem.innerHTML = shopList[i];
     //inserisco l'elemento "li" nella lista in HTML
     containerList.append(listitem);
-    i++
+    i++;
 }
