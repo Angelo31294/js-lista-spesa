5066c31bf34465479d28326d9ff6f01f7bb16332
Angelo31294 <luongoangelo1@gmail.com>
2022-10-27 15:32:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use stricy"
+// definisco lista della spesa
+const shopList =["Pane", "Latte", "Acqua", "Pasta", "Carne"]
+
+const containerList = document.getElementById("list");
+//Creo un elemento "li" per ogni elemento dell'array
+let i = 0;
+
+while(i = shopList.length){
+    const listitem = document.createElement("li");
+    listitem.innerHTML = shopList[i];
+    //inserisco l'elemento "li" nella lista in HTML
+    containerList.append(listitem);
+    i++
+}
